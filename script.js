1a9d96ec0db938c67b87a2d5756a3f381e43cbb3
bipul kumar hansdah <75181701+bipulhk@users.noreply.github.com>
2023-12-12 16:33:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let arr = [1,2,3,4,5,6];
+
+// let newArr = arr.map(function(currentItem, index, arr){
+//     console.log(`the current item is ${currentItem}, index ${index}`);
+//     return currentItem*2
+// })
+
+// console.log(newArr);
+
+
